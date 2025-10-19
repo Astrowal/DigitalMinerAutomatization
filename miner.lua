8fcb29dc1b21cdfe7fc59dcb254152a1430eb0c9
Astrowal <104444257+Astrowal@users.noreply.github.com>
2025-10-19 05:34:02 +0200

    Refactor miner.lua for Minecraft 1.20.1 compatibility
    Updated comments and functionality for better clarity and compatibility with Minecraft 1.20.1. Adjusted chat messages for mining progress.

--- miner.lua
+++ miner.lua
@@ -1,8 +1,16 @@
 -- Computercraft script: 
 
--- Mekanism Digital Miner Automator version 2.1 FIXED
--- ChatBox + Prozent-Updates funktionieren jetzt!
--- Original by MartiNJ409 - https://github.com/martinjanas
+-- Mekanism Digital Miner Automator version 2.1 for Minecraft 1.20.1 
+-- (or maybe even anything above 1.12.2 with slight edits to the Blocks section) 
+-- by MartiNJ409 - https://github.com/martinjanas
+-- This script can be found in my repository - https://github.com/martinjanas/DigitalMinerAutomatization
+ 
+-- This script places & destroys miner, energy block, storage block, chatbox and chunkloader.
+-- It can also output miner status to the chat, visible to anyone on servers, can be turned off below in Settings.
+-- Also works on AdvancedPeriperals's chunky turtle, so no need for chunkloader.
+ 
+-- Just place all the required blocks into the turtle.
+-- No need to use chunkloader if you are using chunky turtle, chat box is not mandatory.
  
 -- User Settings Area --
 Settings = {}
@@ -34,69 +42,66 @@ function main(i)
 
    os.sleep(0.15)
 
-      if GlobalVars.m_pMiner then
-        GlobalVars.m_pMiner.start()
-        print("Miner gestartet!")
-        
-        -- Test Chat
-        if GlobalVars.m_pChatBox and Settings.SEND_TO_CHAT then
-            GlobalVars.m_pChatBox.sendMessage("Runde " .. i .. " gestartet!", "Miner")
-        end
-        
-        local to_mine_cached = GlobalVars.m_pMiner.getToMine()
-        
-        while GlobalVars.m_pMiner.isRunning() do
-            local to_mine = GlobalVars.m_pMiner.getToMine()
-            local seconds = (to_mine * 0.5)
-            
+   if GlobalVars.m_pMiner then
+      GlobalVars.m_pMiner.start()
+
+      local to_mine_cached = GlobalVars.m_pMiner.getToMine()
+
+      while GlobalVars.m_pMiner.isRunning() do
+         local to_mine = GlobalVars.m_pMiner.getToMine()
+         local seconds = (to_mine * 0.5)
+
+         if GlobalVars.m_pChatBox and Settings.SEND_TO_CHAT then
+            local percentage = (to_mine / to_mine_cached) * 100
+            percentage = math.floor(percentage)
+
+            if utils_percentage_in_range(percentage, 80, 1) then
+               local text = string.format("20%% of Blocks Mined (%d/%d)", to_mine, to_mine_cached)
+               GlobalVars.m_pChatBox.sendMessage(text, "Miner")
+               os.sleep(2)
+            end
+
+            if utils_percentage_in_range(percentage, 50, 1) then
+               local text = string.format("50%% of Blocks Mined (%d/%d)", to_mine, to_mine_cached)
+               GlobalVars.m_pChatBox.sendMessage(text, "Miner")
+               os.sleep(2)
+            end
+
+            if utils_percentage_in_range(percentage, 30, 1) then
+               local text = string.format("70%% of Blocks Mined (%d/%d)", to_mine, to_mine_cached)
+               GlobalVars.m_pChatBox.sendMessage(text, "Miner")
+               os.sleep(2)
+            end
+         end
+
+         if to_mine % 5 then
+            local text = string.format("To mine: %d, ETA: %s", to_mine, utils_get_time(seconds))
+		      print(text)
+         end
+
+         if (to_mine == 0) then
             if GlobalVars.m_pChatBox and Settings.SEND_TO_CHAT then
-                local percentage = (to_mine / to_mine_cached) * 100
-                percentage = math.floor(percentage)
-                
-                if utils_percentage_in_range(percentage, 80, 1) then
-                    local text = string.format("20%% abgebaut (%d/%d)", to_mine_cached - to_mine, to_mine_cached)
-                    GlobalVars.m_pChatBox.sendMessage(text, "Miner")
-                    os.sleep(2)
-                end
-                
-                if utils_percentage_in_range(percentage, 50, 1) then
-                    local text = string.format("50%% abgebaut (%d/%d)", to_mine_cached - to_mine, to_mine_cached)
-                    GlobalVars.m_pChatBox.sendMessage(text, "Miner")
-                    os.sleep(2)
-                end
-                
-                if utils_percentage_in_range(percentage, 30, 1) then
-                    local text = string.format("70%% abgebaut (%d/%d)", to_mine_cached - to_mine, to_mine_cached)
-                    GlobalVars.m_pChatBox.sendMessage(text, "Miner")
-                    os.sleep(2)
-                end
+               local text = string.format("Done (%d/%d) rounds", i, Settings.MAX_CHUNKS)
+               GlobalVars.m_pChatBox.sendMessage(text, "Miner")
+               os.sleep(1)
             end
-            
-            if to_mine % 5 == 0 then
-                local text = string.format("Noch zu minen: %d, ETA: %s", to_mine, utils_get_time(seconds))
-                print(text)
-            end
-            
-            if (to_mine == 0) then
-                if GlobalVars.m_pChatBox and Settings.SEND_TO_CHAT then
-                    local text = string.format("Fertig! Runde %d/%d abgeschlossen", i, Settings.MAX_CHUNKS)
-                    GlobalVars.m_pChatBox.sendMessage(text, "Miner")
-                    os.sleep(1)
-                end
                 
-                if i == Settings.MAX_CHUNKS and GlobalVars.m_pChatBox and Settings.SEND_TO_CHAT then
-                    GlobalVars.m_pChatBox.sendMessage("Alle Runden fertig! Bitte abholen!", "Miner")
-                    os.sleep(1)
-                end
-                
-                utils_destroy_blocks()
-                os.sleep(2)
-                utils_go_one_chunk()
+            if i == Settings.MAX_CHUNKS and GlobalVars.m_pChatBox and Settings.SEND_TO_CHAT then
+               local text = string.format("Pick me up! I am finished!")
+               GlobalVars.m_pChatBox.sendMessage(text, "Miner")
+               os.sleep(1)
             end
-            
-            os.sleep(0.5)
-        end
-    end
+
+            utils_destroy_blocks(GlobalVars)
+
+            os.sleep(2)
+
+            utils_go_one_chunk()
+         end
+
+         os.sleep(0.5)
+      end
+   end
 end
 
 function setup()
@@ -105,8 +110,7 @@ function setup()
       sleep(1)
    end
 
-   -- FIXED: Nutze deine GitHub-Version mit ChatBox-Fix
-   shell.run("wget https://raw.githubusercontent.com/Astrowal/DigitalMinerAutomatization/refs/heads/main/utils.lua")
+   shell.run("wget https://raw.githubusercontent.com/Astrowal/DigitalMinerAutomatization/main/utils.lua")
 end
 
 done = false
@@ -123,6 +127,3 @@ for i = 1, Settings.MAX_CHUNKS do
     
    main(i)
 end
-
-
-
